af2ecd721e6274bbb93f00ca63f052476d67048f
Jordan Sutton <jordan.a.sutton@gmail.com>
2018-01-19 17:02:54 +0000

    java

---
+++ days2ndWeek/javascript/game.js
@@ -0,0 +1,62 @@
+let score = 0;
+let scorebox = document.getElementById("score");
+let questionbox = document.getElementById("question");
+let answerform = document.getElementById("myForm");
+
+function setAdditionGame() {
+    document.getElementById("myForm").setAttribute("data-gametype", "addition");
+    additionQuiz();
+}
+
+function setSubtractionGame() {
+    document.getElementById("myForm").setAttribute("data-gametype", "subtraction");
+    subtractionQuiz();
+}
+
+function setMultiplicationGame() {
+    document.getElementById("myForm").setAttribute("data-gametype", "multiplication");
+    multiplicationQuiz();
+}
+
+
+function checkAnswer() {
+    let gametype = document.getElementById("myForm").getAttribute("data-gametype");
+    if (answerform["answer"].value == answerform["rightAnswer"].value) {
+       alert("Hey! You got it right!");
+       score++;
+    } else {
+        alert("Maybe next time, try again!");
+    }
+    scorebox.textContent = score;
+    if(gametype == "addition") {
+        additionQuiz();
+    }
+    if(gametype == "subtraction") {
+        subtractionQuiz();
+    }
+    if(gametype == "multiplication") {
+        multiplicationQuiz();
+    }
+}
+
+function additionQuiz() {
+    let num1 = Math.floor(Math.random() * 50);
+    let num2 = Math.floor(Math.random() * 50);
+    questionbox.textContent = "what is: " + num1 + " + " + num2 + "?";
+    answerform["rightAnswer"].value = (num1 + num2);
+}
+function subtractionQuiz() {
+    let num1 = Math.floor(Math.random() * 50);
+    let num2 = Math.floor(Math.random() * 50);
+    questionbox.textContent = "what is: " + num1 + " - " + num2 + "?";
+    answerform["rightAnswer"].value = (num1 - num2);
+}
+function multiplicationQuiz() {
+    let num1 = Math.floor(Math.random() * 50);
+    let num2 = Math.floor(Math.random() * 50);
+    questionbox.textContent = "what is: " + num1 + " * " + num2 + "?";
+    answerform["rightAnswer"].value = (num1 * num2);
+}
+
+additionQuiz();
+
